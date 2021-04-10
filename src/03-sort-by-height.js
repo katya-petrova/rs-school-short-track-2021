2e6c2e3c39dda58e322532f96c60844e2e3fbf68
katya-petrova <walki-disi@ya.ru>
2021-04-10 16:12:25 +0300

    feat: add solution

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,30 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const sorted = [];
+  const array = arr;
+  const indices = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (array[i] !== -1) {
+      sorted.push(arr[i]);
+      sorted.sort((a, b) => a - b);
+    }
+  }
+  const x = array.map((item) => (item !== -1 ? 'z' : item));
+  x.forEach((yourArray, index) => {
+    if (yourArray === 'z') {
+      indices.push(index);
+    }
+  });
+  for (let i = 0; i < x.length; i++) {
+    for (let j = 0; j < indices.length; j++) {
+      if (x[i] === 'z') {
+        x.splice(indices[j], 1, sorted[j]);
+      }
+    }
+  }
+  return x;
 }
 
 module.exports = sortByHeight;
--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,13 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const myFunc = (num) => Number(num);
+  const arr = Array.from(String(n), myFunc);
+  const min = Math.min.apply(null, arr);
+  arr.splice(arr.indexOf(min), 1);
+  const result = arr.join('');
+  return +result;
 }
 
 module.exports = deleteDigit;
