7500a753b1a0ac9cbfb9683022d98b524e4d076e
katya-petrova <walki-disi@ya.ru>
2021-04-12 20:41:43 +0300

    feat: add solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,20 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (char === str[i + 1]) {
+      count++;
+    } else {
+      result += count + char;
+      count = 1;
+    }
+  }
+
+  return result.replace(/1/g, '');
 }
 
 module.exports = encodeLine;
