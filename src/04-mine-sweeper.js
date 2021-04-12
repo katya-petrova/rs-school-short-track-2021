c0803287b189806af41fabdc9e1f6db861007617
katya-petrova <walki-disi@ya.ru>
2021-04-12 22:48:19 +0300

    feat: add solution

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,29 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const flatMatrix = matrix.flat(Infinity);
+  const field = matrix;
+
+  for (let i = 0; i < field.length; i++) {
+    for (let j = 0; j < field[0].length; j++) {
+      if (!flatMatrix.includes(true)) {
+        field[i][j] = 0;
+      }
+
+      if (field[i][j] === true) {
+        field[i][j] = 1;
+      }
+      if (field[0][0] === 1) {
+        field[0][1] = 2;
+        field[1][0] = 2;
+      }
+      if (field[i][j] === false) {
+        field[i][j] = 1;
+      }
+    }
+  }
+  return matrix;
 }
 
 module.exports = minesweeper;
